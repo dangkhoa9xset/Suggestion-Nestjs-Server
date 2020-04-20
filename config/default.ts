33f48e09691c7c0b972f932d9dc082a348fbe63c
Khoa Phan <khoa.phan.9xset@gmail.com>
2020-04-20 17:24:07 +0700

    commit

--- config/default.ts
+++ config/default.ts
@@ -1,6 +1,6 @@
 export default {
     HOST: 'http://localhost',
     PORT: 3000,
-    MONGO_URI: 'mongodb://localhost/suggestion-app',
+    MONGO_URI: 'mongodb://mongo/suggestion-app',
     JWT_KEY: "ThesecretoftheUniverse'seYEwillNeverberevealed",
 };
